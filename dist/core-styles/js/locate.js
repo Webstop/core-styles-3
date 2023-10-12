23fa2105bb3fa52dab07e4e424601e692a429b0c
Greg Hemphill <greg@webstop.com>
2023-10-12 16:36:16 -0400

    locate.js bug fixes

--- dist/core-styles/js/locate.js
+++ dist/core-styles/js/locate.js
@@ -1,30 +1,30 @@
 "use strict";
 
 // Geolocation
-
-
 (function(){
 
-  function locate(trigger, target, message, hide_trigger, display_messages, redirect_url) {
+  function locate(trigger, target, message, hide_trigger, display_messages, redirect_url, action_url) {
 
     function success(position) {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      let load_url =`${window.webstop.webHost}/retailers/${window.webstop.retailerID}/stores?display=results-only&latitude=${latitude}&longitude=${longitude}&url=${redirect_url}`;
-      load(target, load_url);
+      const default_action_url = `${window.webstop.webHost}/retailers/${window.webstop.retailerID}/stores?display=results-only&latitude=${latitude}&longitude=${longitude}`;
+      let action_url = action_url || default_action_url;
+      if(action_url == ''){ action_url = default_action_url; }
+      if(redirect_url != ''){ action_url += `&url=${redirect_url}`; }
+      load(target, action_url);
       if(display_messages){ message.textContent = ''; }
       if(hide_trigger){ trigger.classList.add('d-none'); }
     }
 
     function error(error) {
-      if(display_messages){ message.textContent = `Unable to retrieve your location. ${display_messages}`; }
+      if(display_messages){ message.textContent = `Unable to retrieve your location.`; }
       console.error(`Geolocation Error. Code: ${error.code}. Message: ${error.message}`);
     }
 
     if(!navigator.geolocation) {
       if(display_messages){ message.textContent = "Geolocation is not supported by your browser."; }
     } else {
-      alert(window.webstop.apiHost);
       if(display_messages){ message.textContent = 'Locating…'; }
       navigator.geolocation.getCurrentPosition(success, error);
     }
@@ -35,11 +35,16 @@
     let message = document.querySelector(trigger.getAttribute('data-locate-message'));
     let target = document.querySelector(trigger.getAttribute('data-locate-target'));
     let hide_trigger = trigger.hasAttribute('data-locate-hide-me');
-    let redirect_url = trigger.getAttribute('data-locate-redirect-url');
-
+    let locate_on_load = trigger.hasAttribute('data-locate-on-load');
+    let action_url = ''
+    let has_action_url = trigger.hasAttribute('data-locate-action-url');
+    if(has_action_url){ action_url = trigger.getAttribute('data-locate-action-url'); }
+    let redirect_url = ''
+    let has_redirect_url = trigger.hasAttribute('data-locate-redirect-url');
+    if(has_redirect_url){ redirect_url = trigger.getAttribute('data-locate-redirect-url'); }
     trigger.addEventListener("click", function(event){locate(trigger, target, message, hide_trigger, true, redirect_url)});
     // the following runs locate on page load, but without hiding the Use My Location button.
-    locate(trigger, target, message, false, false, redirect_url);
+    if(locate_on_load){locate(trigger, target, message, false, false, redirect_url, action_url);}
   }
 
   locator();
--- docs/docs/3.0/dist/core-styles/js/locate.js
+++ docs/docs/3.0/dist/core-styles/js/locate.js
@@ -10,7 +10,10 @@
     function success(position) {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      let load_url =`${window.webstop.webHost}/retailers/${window.webstop.retailerID}/stores?display=results-only&latitude=${latitude}&longitude=${longitude}&url=${redirect_url}`;
+      let has_url = trigger.hasAttribute('data-locate-redirect-url');
+      let load_url =`${window.webstop.webHost}/retailers/${window.webstop.retailerID}/stores?display=results-only&latitude=${latitude}&longitude=${longitude}`;
+      if(has_url){ load_url += `&url=${trigger.getAttribute('data-locate-redirect-url')}`; }
+      //let load_url =`${window.webstop.webHost}/retailers/${window.webstop.retailerID}/stores?display=results-only&latitude=${latitude}&longitude=${longitude}&url=${redirect_url}`;
       load(target, load_url);
       if(display_messages){ message.textContent = ''; }
       if(hide_trigger){ trigger.classList.add('d-none'); }
@@ -24,7 +27,6 @@
     if(!navigator.geolocation) {
       if(display_messages){ message.textContent = "Geolocation is not supported by your browser."; }
     } else {
-      alert(window.webstop.apiHost);
       if(display_messages){ message.textContent = 'Locating…'; }
       navigator.geolocation.getCurrentPosition(success, error);
     }
@@ -35,11 +37,12 @@
     let message = document.querySelector(trigger.getAttribute('data-locate-message'));
     let target = document.querySelector(trigger.getAttribute('data-locate-target'));
     let hide_trigger = trigger.hasAttribute('data-locate-hide-me');
+    let locate_on_load = trigger.hasAttribute('data-locate-on-load');
     let redirect_url = trigger.getAttribute('data-locate-redirect-url');
 
     trigger.addEventListener("click", function(event){locate(trigger, target, message, hide_trigger, true, redirect_url)});
     // the following runs locate on page load, but without hiding the Use My Location button.
-    locate(trigger, target, message, false, false, redirect_url);
+    if(locate_on_load){locate(trigger, target, message, false, false, redirect_url);}
   }
 
   locator();
