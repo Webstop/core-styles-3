7763b230c65867febdd30e714e10dd9e5ba9c82c
Greg Hemphill <greg@webstop.com>
2026-02-26 11:43:56 -0500

    Deleted versions folder

--- versions/v3.0.10/src/js/main-public-aye.js
+++
@@ -1,7 +0,0 @@
-
-// Core-Styles Analytics Files
-// Must come *after* the core-styles.js or core-styles.min.js file.
-// Only include this file in production, or if you are testing analytics.
-import '../node_modules/ahoy.js/dist/ahoy';
-import './aye';
-
