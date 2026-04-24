fd7251d88fcc5224498df30071aa0596ef30b0d1
Don Sullivan <apologeticdon@gmail.com>
2026-04-24 10:29:12 -0700

    Builds v3.0.20

--- dist/js/core-styles-admin.js
+++ dist/js/core-styles-admin.js
@@ -153,8 +153,10 @@
         let $btn = $(this);
         // submit inputs use val and submit buttons use text.
         if ($btn.is('input')) {
+          $btn.data('original-text', $btn.val());
           $btn.val(loadingText);
         } else {
+          $btn.data('original-text', $btn.html());
           $btn.text(loadingText);
         }
         $btn.prop('disabled', true);
@@ -171,6 +173,16 @@
       $submitButtons.prop('disabled', true);
 
       $target.load(url,data,function(){
+        $submitButtons.each(function() {
+          let $btn = $(this);
+          let originalText = $btn.data('original-text');
+          if ($btn.is('input')) {
+            $btn.val(originalText);
+          } else {
+            $btn.html(originalText);
+          }
+          $btn.prop('disabled', false);
+        });
         if(hasClearOnComplete){ clearOnComplete(clearOnCompleteTarget); }
         if(hasHideOnComplete){ hideOnComplete(hideOnCompleteTarget); }
         if(hasShowOnComplete){ showOnComplete(showOnCompleteTarget); }
